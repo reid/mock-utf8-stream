a882279ee8d22824f99b91f1d05349ce8f99e45f
Reid Burke <me@reidburke.com>
2014-01-28 14:14:56 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# mock-utf8-streams
+
+Mock readable and writable streams for stdin and stdout UTF-8 text. Only basic operations are mocked. Useful for testing command-line programs.
+
+## Author & License
+
+BSD. See LICENSE for details and copyright information.
---
+++ test/test.js
@@ -0,0 +1,88 @@
+var chai = require("chai");
+var assert = chai.assert;
+var streams = require("../index");
+
+describe("MockReadableStream", function () {
+    it("should implement setEncoding as noop", function () {
+        var mock = new streams.MockReadableStream();
+        assert.doesNotThrow(function () {
+            mock.setEncoding("utf8");
+        });
+    });
+    it("should implement pause as noop", function () {
+        var mock = new streams.MockReadableStream();
+        assert.doesNotThrow(function () {
+            mock.pause();
+        });
+    });
+    it("should implement resume as noop", function () {
+        var mock = new streams.MockReadableStream();
+        assert.doesNotThrow(function () {
+            mock.resume();
+        });
+    });
+    it("should implement destroy as noop", function () {
+        var mock = new streams.MockReadableStream();
+        assert.doesNotThrow(function () {
+            mock.destroy();
+        });
+    });
+    it("should not be a TTY", function () {
+        var mock = new streams.MockReadableStream();
+        assert.isFalse(mock.isTTY);
+    });
+    it("should emit data on write", function (done) {
+        var fixture = "dogcow";
+        var mock = new streams.MockReadableStream();
+        mock.once("data", function (data) {
+            assert.isString(data);
+            assert.strictEqual(data, fixture);
+            done();
+        });
+        mock.write(fixture);
+    });
+    it("should emit string data on Buffer write", function (done) {
+        var fixture = "dogcow";
+        var mock = new streams.MockReadableStream();
+        mock.once("data", function (data) {
+            assert.isString(data);
+            assert.strictEqual(data, fixture);
+            done();
+        });
+        mock.write(new Buffer(fixture, "utf8"));
+    });
+});
+describe("MockWritableStream", function () {
+    it("should emit data on write", function (done) {
+        var fixture = "dogcow";
+        var mock = new streams.MockWritableStream();
+        mock.once("data", function (data) {
+            assert.isString(data);
+            assert.strictEqual(data, fixture);
+            done();
+        });
+        mock.write(fixture);
+    });
+    it("should emit string data on Buffer write", function (done) {
+        var fixture = "dogcow";
+        var mock = new streams.MockWritableStream();
+        mock.once("data", function (data) {
+            assert.isString(data);
+            assert.strictEqual(data, fixture);
+            done();
+        });
+        mock.write(new Buffer(fixture, "utf8"));
+    });
+    it("expect should trigger for expectedString", function (done) {
+        var expectedString = "dogcow";
+        var mock = new streams.MockWritableStream();
+        mock.expect(expectedString, function (err, data) {
+            assert.isNull(err); 
+            assert.isString(data);
+            assert.strictEqual(data, "foobar" + expectedString, "Intermediate data not given to expect callback");
+            done();
+        });
+        mock.write("foo");
+        mock.write("bar" + expectedString);
+    });
+});
